4c7474507db0c85431fd426fe29ef4d5a09ab21a
Bram De Coninck <bram.deconinck@inthepocket.com>
2021-02-21 21:31:23 +0100

    Add Zenject to the project and start cleaning up the code.

---
+++ Assets/Code/Core/Dialog/Impl/DialogService.cs
@@ -0,0 +1,12 @@
+﻿using Project.Core.Dialog.Interface;
+
+namespace Project.Core.Dialog.Impl
+{
+    public class DialogService : IDialogService
+    {
+        public void ShowToast(string message)
+        {
+            SSTools.ShowMessage(message, SSTools.Position.bottom, SSTools.Time.twoSecond);
+        }
+    }
+}
---
+++ Assets/Code/Di/GameInstaller.cs
@@ -0,0 +1,14 @@
+﻿using Project.Core.Dialog.Impl;
+using Project.Core.Dialog.Interface;
+using Zenject;
+
+namespace Project.Di
+{
+    public class GameInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<IDialogService>().To<DialogService>().AsSingle();
+        }
+    }
+}
