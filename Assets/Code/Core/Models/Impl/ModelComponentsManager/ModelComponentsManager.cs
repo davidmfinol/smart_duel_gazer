f9de3d990c39a34a1483befd890b8316ddb7d4d8
SubtleDragon057 <devan_ninjapc@yahoo.ca>
2021-06-24 09:27:44 -0600

    Fixed Typo in Model Components Manager

--- Assets/Code/Core/Models/Impl/ModelComponentsManager/ModelComponentsManager.cs
+++ Assets/Code/Core/Models/Impl/ModelComponentsManager/ModelComponentsManager.cs
@@ -16,7 +16,7 @@ namespace AssemblyCSharp.Assets.Code.Core.Models.Impl.ModelComponentsManager
         private SkinnedMeshRenderer[] _renderers;
         private ModelSettings _settings;
         private string _zone;
-        private bool areRenderersEnabled;
+        private bool _areRenderersEnabled;
 
         #region Constructor
 
@@ -99,7 +99,7 @@ namespace AssemblyCSharp.Assets.Code.Core.Models.Impl.ModelComponentsManager
             }
 
             _renderers.SetRendererVisibility(true);
-            areRenderersEnabled = true;
+            _areRenderersEnabled = true;
             _animator.SetBool(AnimatorParameters.DefenceBool, false);
             _animator.SetTrigger(AnimatorParameters.SummoningTrigger);
         }
@@ -122,7 +122,7 @@ namespace AssemblyCSharp.Assets.Code.Core.Models.Impl.ModelComponentsManager
             }
 
             _renderers.SetRendererVisibility(state);
-            areRenderersEnabled = state;
+            _areRenderersEnabled = state;
         }
 
         private void DestroyMonster(string zone)
