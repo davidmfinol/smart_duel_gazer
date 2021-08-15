2cbfa81ab2b3ad1f20e2103b1a31fbdf4e608e00
SubtleDragon057 <devan_ninjapc@yahoo.ca>
2021-08-15 10:14:57 -0600

    Bug Fix

--- Assets/Code/UI Components/General/ScreenOrientation/SetPortrait.cs
+++ Assets/Code/UI Components/General/ScreenOrientation/SetPortrait.cs
@@ -1,9 +1,12 @@
 using Code.Core.Screen;
+using UnityEngine;
+using Zenject;
 
-public class SetPortrait
+public class SetPortrait : MonoBehaviour
 {
     private IScreenService _screenService;
 
+    [Inject]
     public SetPortrait(IScreenService screenService)
     {
         _screenService = screenService;
