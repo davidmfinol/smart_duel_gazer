c8adc28232956f060c07fc8f080360cb949f5ea2
David Finol <davidmfinol@gmail.com>
2022-02-19 19:59:13 -0600

    Work on Tilt Five

--- Assets/Code/Features/SpeedDuel/EventHandlers/Placement/TiltFive/TiltFivePlacementEventHandler.cs
+++ Assets/Code/Features/SpeedDuel/EventHandlers/Placement/TiltFive/TiltFivePlacementEventHandler.cs
@@ -2,7 +2,6 @@ using Code.Core.DataManager;
 using Code.Core.Logger;
 using Code.Features.SpeedDuel.PrefabManager;
 using Code.Features.SpeedDuel.PrefabManager.Prefabs.Playfield.Scripts;
-// TODO: using Code.Wrappers.WrapperNreal;
 using UnityEngine;
 using Zenject;
 
@@ -18,15 +17,103 @@ namespace Code.Features.SpeedDuel.EventHandlers.Placement.Nreal
         private IDataManager _dataManager;
         private IPlayfieldEventHandler _playfieldEventHandler;
         private PlayfieldComponentsManager.Factory _playfieldFactory;
-        // TODO: private INrealSessionManagerWrapper _nrealSessionManager;
         private IAppLogger _logger;
 
-        // TODO: private NrealPlaneManager _planeManager;
         private GameObject _prefabManager;
         
         private GameObject _speedDuelField;
-        private Pose _placementPose;
-        private bool _objectPlaced;
+
+        #region Construct
+
+        [Inject]
+        public void Construct(
+            IDataManager dataManager,
+            IPlayfieldEventHandler playfieldEventHandler,
+            PlayfieldComponentsManager.Factory playfieldFactory,
+            IAppLogger logger)
+        {
+            _dataManager = dataManager;
+            _playfieldEventHandler = playfieldEventHandler;
+            _playfieldFactory = playfieldFactory;
+            _logger = logger;
+        }
+
+        #endregion
+
+        #region LifeCycle
+
+        private void Awake()
+        {
+            GetObjectReferences();
+            _playfieldEventHandler.OnRemovePlayfield += RemovePlayfield;
+        }
+
+        private void Start()
+        {
+            PlacePlayfield();
+        }
+
+        private void OnDestroy()
+        {
+            _playfieldEventHandler.OnRemovePlayfield -= RemovePlayfield;
+        }
+
+        #endregion
+
+        private void GetObjectReferences()
+        {
+            _prefabManager = FindObjectOfType<SpeedDuelPrefabManager>().gameObject;
+        }
+
+        #region Playfield
+
+        private void PlacePlayfield()
+        {
+            _logger.Log(Tag, "PlacePlayfield()");
+
+            placementIndicator.SetActive(false);
+
+            var playfield = _dataManager.GetPlayfield();
+            if (playfield == null)
+            {
+                CreatePlayfield();
+            }
+            else
+            {
+                playfield.transform.SetPositionAndRotation(placementIndicator.transform.position, placementIndicator.transform.rotation);
+            }
+
+            _playfieldEventHandler.ActivatePlayfield();
+        }
+
+        private void CreatePlayfield()
+        {
+            _logger.Log(Tag, "CreatePlayfield()");
+
+            _speedDuelField = _playfieldFactory.Create(playfieldPrefab).gameObject;
+            _dataManager.SavePlayfield(_speedDuelField);
+
+            // Move Playfield to Scene Root rather than Zenject Project Context
+            _speedDuelField.transform.SetParent(transform);
+            _speedDuelField.transform.SetPositionAndRotation(placementIndicator.transform.position, placementIndicator.transform.rotation);
+
+            // Make Prefab Manager a child of Playfield for proper model scaling
+            _prefabManager.transform.SetParent(_speedDuelField.transform);
+            _prefabManager.transform.SetPositionAndRotation(_speedDuelField.transform.position,
+                _speedDuelField.transform.rotation);
+
+            // Scale up Playfield to fill up the GameBoard
+            _speedDuelField.transform.localScale = new Vector3(2, 2, 2);
+        }
+        
+        #endregion
+
+        private void RemovePlayfield()
+        {
+            _logger.Log(Tag, "RemovePlayfield()");
+
+            placementIndicator.SetActive(true);
+        }
         
     }
 }
